a95b9c632ce4d4a67478d382052b4b7211ac671d
79096 <a.malakhinsky@gmail.com>
2024-06-11 19:10:02 +0300

    create

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from src.db_manager import DBManager
 from src.get_vacancy import get_vacancies_list, get_companies, get_vacancies
 from src.utils import create_db, insert_data
 
-# Получение параметров конфигурации
+
+# Получение параметров конфигурации1
 params = config()
 
 # Получение данных о вакансиях
@@ -21,7 +22,6 @@ conn = psycopg2.connect(dbname='best_vacancies', **params)
 # Вставка данных о вакансиях в базу данных
 insert_data(conn, vacancies)
 
-
 def main():
     """
     Основная функция, которая управляет взаимодействием с пользователем для выполнения запросов к базе данных.
@@ -32,8 +32,7 @@ def main():
     while True:
         print(f'Выберите запрос либо введите слово "стоп": \n'
               f'1 - Список всех компаний и количество вакансий у каждой компании\n'
-              f'2 - Cписок всех вакансий с указанием названия компании, названия вакансии, '
-              f'зарплаты и ссылки на вакансию\n'
+              f'2 - Cписок всех вакансий с указанием названия компании, названия вакансии, зарплаты и ссылки на вакансию\n'
               f'3 - Средняя зарплата по вакансиям\n'
               f'4 - Список всех вакансий, у которых зарплата выше средней по всем вакансиям\n'
               f'5 - Список всех вакансий, в названии которых содержится запрашиваемое слово\n')
@@ -45,4 +44,21 @@ def main():
             print(f"Список всех компаний и количество вакансий у каждой компании: {companies_vacancies_count}")
         elif user_request == '2':
             vacancy_list = db_manager.get_all_vacancies()
-            print(f"Список всех вакансий с указанием названия компании, вакансии, зарплаты и ссылки на вакансию:")
+            print(f"Список всех вакансий с указанием названия компании, вакансии, зарплаты и ссылки на вакансию: {vacancy_list}")
+        elif user_request == '3':
+            avg_salary = db_manager.get_avg_salary()
+            print(f"Средняя зарплата по вакансиям: {avg_salary}")
+        elif user_request == '4':
+            vacancies_with_higher_salary = db_manager.get_vacancies_with_higher_salary()
+            print(f"Список всех вакансий, у которых зарплата выше средней по всем вакансиям: {vacancies_with_higher_salary}")
+        elif user_request == '5':
+            user_input = input(f'Введите слово: ')
+            vacancies_with_keyword = db_manager.get_vacancies_with_keyword(user_input)
+            print(f"Список всех вакансий, в названии которых содержится '{user_input}': {vacancies_with_keyword}")
+        elif user_request == 'стоп':
+            break
+        else:
+            print("Введён неверный запрос")
+
+if __name__ == "__main__":
+    main()
