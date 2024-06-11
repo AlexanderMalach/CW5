30a8e265dcb826282a685f2f9d993060ad78d55a
79096 <a.malakhinsky@gmail.com>
2024-06-11 20:12:25 +0300

    .

--- src/utils.py
+++ src/utils.py
@@ -8,17 +8,17 @@ vacancies = get_vacancies_list(data)
 
 def create_db(name: str, params: dict) -> str:
     """
-    Создает базу данных и таблицы для сохранения данных о компаниях и вакансиях.
+    СозБД и таблицы для сохранения данных о компаниях и вакансиях.
 
     :param name: Имя создаваемой базы данных.
     :type name: str
     :param params: Параметры подключения к базе данных (например, пользователь, пароль, хост).
     :type params: dict
-    :return: Сообщение об успешном создании базы данных и таблиц или сообщение об ошибке.
+    :return: Сообщение об успешном создании базы данных и таблиц или об ошибке.
     :rtype: str
     """
     try:
-        # Подключение к основной базе данных для создания новой базы данных
+        # Подключение к основной базе данных для создания новой БД
         conn = psycopg2.connect(dbname='postgres', **params)
         conn.autocommit = True
         cur = conn.cursor()
@@ -51,14 +51,14 @@ def create_db(name: str, params: dict) -> str:
         return f"Произошла ошибка: {e}"
 
 
-def insert_data(conn, vacancies: list[dict]) -> None:
+def insert_data(conn, vacancy: list[dict]) -> None:
     """
-    Сохраняет данные о компаниях и вакансиях в базе данных.
+    Сохраняет данные о компаниях и вакансиях в БД.
 
     :param conn: Подключение к базе данных.
     :type conn: psycopg2.extensions.connection
-    :param vacancies: Список словарей с информацией о вакансиях.
-    :type vacancies: list[dict]
+    :param vacancy: Список словарей с информацией о вакансиях.
+    :type vacancy: list[dict]
     """
     insert_employer_query = """
     INSERT INTO employers (company_id, company_name, company_url) 
@@ -71,12 +71,12 @@ def insert_data(conn, vacancies: list[dict]) -> None:
     """
     try:
         with conn.cursor() as cur:
-            for record in vacancies:
+            for record in vacancy:
                 cur.execute(insert_employer_query,
                             (record['company_id'], record['company_name'], record['company_url']))
                 cur.execute(insert_vacancy_query, (
-                record['company_name'], record['job_title'], record['link_to_vacancy'], record['salary_from'],
-                record['currency'], record['description'], record['requirement']))
+                    record['company_name'], record['job_title'], record['link_to_vacancy'], record['salary_from'],
+                    record['currency'], record['description'], record['requirement']))
             conn.commit()
     except Exception as e:
         conn.rollback()
